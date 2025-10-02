c469c17d3050307626fffc005b1022f777b4a894
catarinasahyoun <125618978+catarinasahyoun@users.noreply.github.com>
2025-10-01 23:24:57 -0400

    Update __init__.py

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -1,7 +1,3 @@
 """Pages module for different application views."""
 
-from .tool_page import ToolPage
-from .results_page import ResultsPage
-from .user_guide_page import UserGuidePage
-from .settings_page import SettingsPage
-from .versions_page import VersionsPage
+__all__ = []
